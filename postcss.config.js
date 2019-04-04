dd2f4077ba0aeaef761b441631c870f02bfbd856
zty <zhangtengyue@qbb6.com>
2019-04-04 15:27:37 +0800

    postcss hmr

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   plugins: [
+    require('postcss-url')(),
+    require('postcss-import')(),
     require('cssnano')()
   ]
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,12 @@ const cleanWebpackPlugin = require('clean-webpack-plugin')
 // const extractTextPlugin = require("extract-text-webpack-plugin") //用来抽离单独抽离css文件
 const miniCssExtractPlugin = require("mini-css-extract-plugin") //用来抽离单独抽离css文件
 const copyWebpackPlugin = require("copy-webpack-plugin")
+const webpack = require('webpack')
 const devEnv = process.env.NODE_ENV !== 'production';
+
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+}
 
 module.exports = {
   entry: {
@@ -56,8 +61,15 @@ module.exports = {
       }
     ]
   },
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      '@': resolve('src')
+    }
+  },
   plugins: [ // 插件
     // new extractTextPlugin("styles.css"),
+    new webpack.HotModuleReplacementPlugin(),
     new cleanWebpackPlugin(), // 删除文件 保留新文件
     new copyWebpackPlugin([{
       from: path.resolve(__dirname, 'public/static'), 
@@ -84,7 +96,7 @@ module.exports = {
     port: 8088,
     host: 'localhost',
     overlay: true, // 浏览器会显示编译错误
-    inline:true, // 默认为true, 意思是，在打包时会注入一段代码到最后的js文件中，用来监视页面的改动而自动刷新页面,当为false时，网页自动刷新的模式是iframe，也就是将模板页放在一个frame中
-    hot:true
+    inline: true, // 默认为true, 意思是，在打包时会注入一段代码到最后的js文件中，用来监视页面的改动而自动刷新页面,当为false时，网页自动刷新的模式是iframe，也就是将模板页放在一个frame中
+    hot: true
   }
 }
